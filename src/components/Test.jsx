1160501fe70acabca46f60516dbf4553bdee15a2
Akbar Rivaldy <akbar22rivaldy@gmail.com>
2022-09-30 17:46:49 +0700

    initial commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+
+export default function Test() {
+  return (
+    <div className="flex justify-center items-center flex-col h-screen space-y-4">
+      <h1 className="text-4xl font-bold">Vite + React + Tailwind</h1>
+      <p className="text-xl font-semibold">Easy setup for vite with react and tailwind to build awesome project.</p>
+      <button className="px-5 py-2 bg-emerald-500 rounded text-slate-50 hover:bg-emerald-600">Get Started</button>
+    </div>
+  )
+}
